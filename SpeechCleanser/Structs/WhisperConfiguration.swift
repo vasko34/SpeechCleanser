f0f33f980aac71fef46055014995244283487468
Vasil Botsev <vaskobotsev@gmail.com>
2025-10-09 06:38:15 +0300

    Reset + Added medium and large-v2 models

--- SpeechCleanser/Structs/WhisperConfiguration.swift
+++ SpeechCleanser/Structs/WhisperConfiguration.swift
@@ -8,13 +8,5 @@
 import Foundation
 
 struct WhisperConfiguration {
-    let windowDuration: TimeInterval
-    let hopDuration: TimeInterval
-    let enableVAD: Bool
     
-    init(windowDuration: TimeInterval, hopDuration: TimeInterval, enableVAD: Bool) {
-        self.windowDuration = windowDuration
-        self.hopDuration = hopDuration
-        self.enableVAD = enableVAD
-    }
 }
--- SpeechCleanser/Structs/WhisperTranscriptionResult.swift
+++ SpeechCleanser/Structs/WhisperTranscriptionResult.swift
@@ -8,14 +8,5 @@
 import Foundation
 
 struct WhisperTranscriptionResult {
-    let transcript: String
-    let windowStartDate: Date
-    let startOffset: TimeInterval
-    let endOffset: TimeInterval
-    let audioDuration: TimeInterval
-    let sampleCount: Int
     
-    var segmentDuration: TimeInterval {
-        max(endOffset - startOffset, 0)
-    }
 }
